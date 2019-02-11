1bfb0203f7f55ca08e16e7c899f2bd326a257b2a
jjchern <jiajia.chern@gmail.com>
2019-02-11 17:00:00 -0600

    Update doc

--- R/data.R
+++ R/data.R
@@ -1,19 +1,19 @@
-#' Medicaid Income Eligibility Limits for Parents, 2002-2015
+#' Medicaid Income Eligibility Limits for Parents, 2002-2018
 #'
-#' @source /url{http://kff.org/medicaid/state-indicator/medicaid-income-eligibility-limits-for-parents-2002-2015}
+#' @source [Medicaid Income Eligibility Limits for Parents, 2002-2018](https://www.kff.org/medicaid/state-indicator/medicaid-income-eligibility-limits-for-parents/)
 "parents"
 
-#' Medicaid Income Eligibility Limits for Childless Adults, 2011-2016
+#' Medicaid Income Eligibility Limits for Childless Adults, 2011-2018
 #'
-#' @source /url{http://kff.org/medicaid/state-indicator/medicaid-income-eligibility-limits-for-other-non-disabled-adults-2011-2015/}
+#' @source [Medicaid Income Eligibility Limits for Other Non-Disabled Adults, 2011-2018](https://www.kff.org/medicaid/state-indicator/medicaid-income-eligibility-limits-for-other-non-disabled-adults/)
 "childless_adults"
 
-#' Medicaid/CHIP Income Eligibility Limits for Pregnant Women, 2003-2015
+#' Medicaid/CHIP Income Eligibility Limits for Pregnant Women, 2003-2018
 #'
-#' @source /url{http://kff.org/medicaid/state-indicator/medicaid-and-chip-income-eligibility-limits-for-pregnant-women-2003-2015/}
+#' @source [Medicaid and CHIP Income Eligibility Limits for Pregnant Women, 2003-2018](https://www.kff.org/medicaid/state-indicator/medicaid-and-chip-income-eligibility-limits-for-pregnant-women/)
 "pregnant_women"
 
-#' Medicaid/CHIP Income Eligibility Limits for Children, 2000-2015
+#' Medicaid/CHIP Income Eligibility Limits for Children, 2000-2018
 #'
-#' @source /url{http://kff.org/data-collection/trends-in-medicaid-income-eligibility-limits/}
+#' @source [Medicaid/CHIP Upper Income Eligibility Limits for Children, 2000-2018](https://www.kff.org/medicaid/state-indicator/medicaidchip-upper-income-eligibility-limits-for-children/)
 "children"
