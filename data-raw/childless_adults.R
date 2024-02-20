95f64efd0bd332217b3c1b970105e8d5b5ea67df
jjchern <jiajia.chern@gmail.com>
2024-02-19 22:45:52 -0500

    Added 2019–2023 data for childless adults

--- data-raw/childless_adults.R
+++ data-raw/childless_adults.R
@@ -3,15 +3,15 @@
 
 library(tidyverse)
 
-read_csv("data-raw/childless_adults.csv", skip = 2) %>%
-  select(-Footnotes) %>%
-  filter(Location != "United States") %>%
+read_csv("data-raw/childless_adults.csv", skip = 2, n_max = 52) |>
+  select(-Footnotes) |>
   rename(state = Location) %>%
-  inner_join(fips::fips, by = "state") %>%
-  select(state, fips, usps, everything()) %>%
-  gather(year, cutoff, -state:-usps) %>%
-  separate(year, c("month", "year")) %>%
-  mutate(cutoff = as.numeric(cutoff * 100)) %>%
+  left_join(fips::fips, by = "state") |>
+  select(state, fips, usps, everything()) |>
+  gather(year, cutoff, -state:-usps) |>
+  separate(year, c("month", "year")) |>
+  mutate(year = as.integer(year)) |>
+  mutate(cutoff = as.numeric(cutoff * 100)) |>
   print() -> childless_adults
 
 usethis::use_data(childless_adults, overwrite = TRUE)
