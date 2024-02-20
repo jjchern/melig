93f3b1992dc03d1e26fc2489b3392fa7291a23ff
jjchern <jiajia.chern@gmail.com>
2024-02-19 22:51:25 -0500

    Updated pipes

--- data-raw/parents.R
+++ data-raw/parents.R
@@ -3,14 +3,14 @@
 
 library(tidyverse)
 
-read_csv("data-raw/parents.csv", skip = 2, n_max = 52) %>%
-  select(-Footnotes) %>%
-  rename(state = Location) %>%
-  left_join(fips::fips, by = "state") %>%
-  select(state, fips, usps, everything()) %>%
-  gather(year, cutoff, -state:-usps) %>%
-  separate(year, c("month", "year")) %>%
-  mutate(cutoff = as.numeric(cutoff) * 100) %>%
+read_csv("data-raw/parents.csv", skip = 2, n_max = 52) |>
+  select(-Footnotes) |>
+  rename(state = Location) |>
+  left_join(fips::fips, by = "state") |>
+  select(state, fips, usps, everything()) |>
+  gather(year, cutoff, -state:-usps) |>
+  separate(year, c("month", "year")) |>
+  mutate(cutoff = as.numeric(cutoff) * 100) |>
   # WA 2002: Data not reported.
   # filter(is.na(cutoff))
   print() -> parents
--- data-raw/pregnant_women.R
+++ data-raw/pregnant_women.R
@@ -3,14 +3,14 @@
 
 library(tidyverse)
 
-read_csv("data-raw/pregnant_women.csv", skip = 2, n_max = 52) %>%
-  select(-Footnotes) %>%
-  rename(state = Location) %>%
-  left_join(fips::fips, by = "state") %>%
-  select(state, fips, usps, everything()) %>%
-  gather(year, cutoff, -state:-usps) %>%
-  separate(year, c("month", "year")) %>%
-  mutate(cutoff = as.numeric(cutoff * 100)) %>%
+read_csv("data-raw/pregnant_women.csv", skip = 2, n_max = 52) |>
+  select(-Footnotes) |>
+  rename(state = Location) |>
+  left_join(fips::fips, by = "state") |>
+  select(state, fips, usps, everything()) |>
+  gather(year, cutoff, -state:-usps) |>
+  separate(year, c("month", "year")) |>
+  mutate(cutoff = as.numeric(cutoff * 100)) |>
   print() -> pregnant_women
 
 usethis::use_data(pregnant_women, overwrite = TRUE)
